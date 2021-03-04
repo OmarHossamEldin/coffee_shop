12c41fdd9a263a02b5207afcab870ee18f0f5101
OmarHossamEldin <okandil273@gmail.com>
2021-03-04 10:36:30 +0200

    understand-the-structure

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'coffeshopudacity.eu', // the auth0 domain prefix
+    audience: 'coffe-shop-udacity', // the audience set for the auth0 app
+    clientId: 'klTK55yZLJozKpj5eCGsFiK8ZYs8YE5W', // the client id generated for the auth0 app
+    callbackURL: 'http://127.0.0.1:8100/', // the base url of the running ionic application. 
   }
 };
